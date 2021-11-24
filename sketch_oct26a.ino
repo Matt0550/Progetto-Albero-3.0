a4d0c55170cc1a81ef675de3196ddb976cf643fa
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2021-11-24 19:19:34 +0100

    Update sketch_oct26a.ino

--- sketch_oct26a.ino
+++ sketch_oct26a.ino
@@ -13,21 +13,39 @@ void setup() {
     digitalWrite(leds[i], HIGH);
   }
   Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
+String stato_led_prima = "collegato";
+
 void loop() {
+  if(Serial1.available()) {
+      Serial.println(Serial1.read());
+  }
   int stato_led = 0;
   int led_verifica_len = sizeof(led_verifica)/sizeof(led_verifica[0]);
   
   for(int i = 0; i < led_verifica_len; i++) {
-    if (digitalRead(led_verifica[i])) {
+    if (digitalRead(led_verifica[i])) { 
       stato_led = digitalRead(led_verifica[i]);
       Serial.println(stato_led);
-      if (stato_led == 1) { // Collegato
+      
+      if (stato_led == 1 && stato_led_prima == "collegato") {
         Serial.println("Scollegato");
         Serial.println((String)"Led "+leds[i]+": "+stato_led);
         digitalWrite(34, HIGH);
         delay(300);
+        Serial1.println((String)"Led_"+leds[i]+"_"+stato_led+"_"+stato_led_prima);
+        stato_led_prima = "scollegato";
+        
+      } else if(stato_led == 1 && stato_led_prima == "scollegato") {
+        Serial.println("Collegato");
+        Serial.println((String)"Led "+leds[i]+": "+stato_led);
+        digitalWrite(34, HIGH);
+        delay(300);
+        Serial1.println((String)"Led_"+leds[i]+"_"+stato_led+"_"+stato_led_prima);
+        stato_led_prima = "collegato";
+
       }
       
     } else {
