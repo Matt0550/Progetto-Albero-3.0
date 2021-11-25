24d7517eaba7cc977e673d54ae138f4db113d0a1
Pall1n <75300377+Pall1n@users.noreply.github.com>
2021-11-25 12:24:45 +0100

    Add files via upload

---
+++ aml_aml.ino
@@ -0,0 +1,20 @@
+int leds[] = {45, 41};
+String test = "";
+
+void setup() {
+  int leds_len = sizeof(leds)/sizeof(leds[0]);
+  for(int i = 0; i < leds_len; i++) {
+    pinMode(leds[i], OUTPUT);
+    digitalWrite(leds[i], HIGH);
+  }
+  Serial.begin(9600);
+  Serial1.begin(115200);
+}
+
+void loop() {
+  while(Serial1.available()){
+    test = test + char(Serial1.read());
+    Serial.println(test);
+  }
+  test = "";
+}
