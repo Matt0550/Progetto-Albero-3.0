00a611799f22acd107d484520713d9944cfe98d4
Pall1n <75300377+Pall1n@users.noreply.github.com>
2021-12-02 11:26:26 +0100

    Add files via upload

--- aml_aml.ino
+++ aml_aml.ino
@@ -1,5 +1,6 @@
-int leds[] = {45, 41};
-String test = "";
+int leds[] = {31, 32, 35, 36, 39, 40, 43, 44, 47, 48, 51, 52};
+int pin, stato;
+String filtro_pin, filtro_stato, segnale;
 
 void setup() {
   int leds_len = sizeof(leds)/sizeof(leds[0]);
@@ -8,13 +9,24 @@ void setup() {
     digitalWrite(leds[i], HIGH);
   }
   Serial.begin(9600);
-  Serial1.begin(115200);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  while(Serial1.available()){
-    test = test + char(Serial1.read());
-    Serial.println(test);
+  if(Serial1.available()){
+    segnale = Serial1.readString();
+    Serial.println(segnale);
+    filtro_pin = segnale[0];
+    if(isdigit(segnale[1])){
+      filtro_pin += segnale[1];
+      filtro_stato = segnale[3];
+      stato = filtro_stato.toInt();
+    }
+    else{
+      filtro_stato = segnale[2];
+      stato = filtro_stato.toInt();
+    }
+    pin = filtro_pin.toInt();
+    digitalWrite(pin, stato);
   }
-  test = "";
 }
