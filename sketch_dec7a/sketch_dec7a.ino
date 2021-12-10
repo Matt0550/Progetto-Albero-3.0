1bffd2bc3c72f3d33e8f4859b7d6c9e77cd97826
Matt0550 <65899974+Matt0550@users.noreply.github.com>
2021-12-10 11:43:28 +0100

    Update sketch_dec7a.ino

--- sketch_dec7a/sketch_dec7a.ino
+++ sketch_dec7a/sketch_dec7a.ino
@@ -1,4 +1,4 @@
-int leds[] = {31, 32, 35, 36, 39, 40, 43, 44, 47, 48, 51, 52};
+int leds[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42};
 int pin, stato;
 String filtro_pin, filtro_stato, segnale;
 int leds_len = sizeof(leds)/sizeof(leds[0]);
